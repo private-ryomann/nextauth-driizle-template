5576ad3f68e3467685f34952c1e16a6a67e1e77a
private-ryomann <ryomann46497@gmail.com>
2024-10-24 23:27:15 +0900

    環境変数ファイルフォーマット

--- src/env/serverEnv.mjs
+++ src/env/serverEnv.mjs
@@ -5,10 +5,10 @@ const serverEnv = {
 	...browserEnv,
 	...envsafe(
 		{
-			DATABASE_URL: url({input: process.env.DATABASE_URL}),
-			AUTH_SECRET: str({input: process.env.AUTH_SECRET}),
-			AUTH_GOOGLE_ID: str({input: process.env.AUTH_GOOGLE_ID}),
-			AUTH_GOOGLE_SECRET: str({input: process.env.AUTH_GOOGLE_SECRET}),
+			DATABASE_URL: url({ input: process.env.DATABASE_URL }),
+			AUTH_SECRET: str({ input: process.env.AUTH_SECRET }),
+			AUTH_GOOGLE_ID: str({ input: process.env.AUTH_GOOGLE_ID }),
+			AUTH_GOOGLE_SECRET: str({ input: process.env.AUTH_GOOGLE_SECRET }),
 		},
 		{
 			strict: true,
