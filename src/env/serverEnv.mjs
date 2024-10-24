18ff3aa44fab4d93409c0b598f4e69210ee193c4
private-ryomann <ryomann46497@gmail.com>
2024-10-24 23:25:18 +0900

    NextAuthとGoogle認証に必要な環境変数の追加

--- src/env/serverEnv.mjs
+++ src/env/serverEnv.mjs
@@ -1,13 +1,14 @@
-import { url, envsafe } from "envsafe";
+import { url, envsafe, str } from "envsafe";
 import { browserEnv } from "./browserEnv.mjs";
 
 const serverEnv = {
 	...browserEnv,
 	...envsafe(
 		{
-			DATABASE_URL: url({
-				input: process.env.DATABASE_URL,
-			}),
+			DATABASE_URL: url({input: process.env.DATABASE_URL}),
+			AUTH_SECRET: str({input: process.env.AUTH_SECRET}),
+			AUTH_GOOGLE_ID: str({input: process.env.AUTH_GOOGLE_ID}),
+			AUTH_GOOGLE_SECRET: str({input: process.env.AUTH_GOOGLE_SECRET}),
 		},
 		{
 			strict: true,
